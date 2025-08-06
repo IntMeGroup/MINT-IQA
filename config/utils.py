b7ec0a17ca3aca1ea4ddbf2970128f7ef4ea1941
Intelligent Media Group <166475589+IntMeGroup@users.noreply.github.com>
2025-08-06 23:54:24 +0800

    Add files via upload

---
+++ config/utils.py
@@ -0,0 +1,115 @@
+import os, shutil
+import torch
+from tensorboardX import SummaryWriter
+from config.options import *
+import torch.distributed as dist
+
+
+""" ==================== Data ======================== """
+
+def collate_fn(batch):
+    return batch
+
+def make_path():
+    return "{}_bs{}_fix={}_lr={}{}".format(opts.savepath, opts.batch_size, opts.fix_rate, opts.lr, opts.lr_decay_style)
+def make_path2():
+    return "{}_bs{}_fix={}_lr={}{}srcc".format(opts.logfile_name, opts.batch_size, opts.fix_rate, opts.lr, opts.lr_decay_style)
+def make_path3():
+    return "{}_bs{}_fix={}_lr={}{}loss".format(opts.logfile_name, opts.batch_size, opts.fix_rate, opts.lr, opts.lr_decay_style)
+def make_path_llm():
+    return "{}_bs{}_fix={}_lr={}{}llm".format(opts.logfile_name, opts.batch_size, opts.fix_rate, opts.lr, opts.lr_decay_style)
+
+""" ==================== Models ======================== """
+
+def save_model(model):
+    save_path = make_path2()
+    if not os.path.isdir(os.path.join(config['checkpoint_base'], save_path)):
+        os.makedirs(os.path.join(config['checkpoint_base'], save_path), exist_ok=True)
+    model_name = os.path.join(config['checkpoint_base'], save_path, 'best_lr={}acc.pt'.format(opts.lr))
+    torch.save(model.state_dict(), model_name)
+def save_model2(model):
+    save_path = make_path2()
+    if not os.path.isdir(os.path.join(config['checkpoint_base'], save_path)):
+        os.makedirs(os.path.join(config['checkpoint_base'], save_path), exist_ok=True)
+    model_name = os.path.join(config['checkpoint_base'], save_path, 'best_lr={}loss.pt'.format(opts.lr))
+def save_model_llm(model):
+    save_path = make_path_llm()
+    if not os.path.isdir(os.path.join(config['checkpoint_base'], save_path)):
+        os.makedirs(os.path.join(config['checkpoint_base'], save_path), exist_ok=True)
+    model_name = os.path.join(config['checkpoint_base'], save_path, 'best_lr={}.pt'.format(opts.lr))
+    torch.save(model.state_dict(), model_name)
+def save_model_srocc(model):
+    save_path = make_path2()
+    if not os.path.isdir(os.path.join(config['checkpoint_base'], save_path)):
+        os.makedirs(os.path.join(config['checkpoint_base'], save_path), exist_ok=True)
+    model_name = os.path.join(config['checkpoint_base'], save_path, 'best_lr={}.pt'.format(opts.lr))
+    torch.save(model.state_dict(), model_name)
+def save_model_loss(model):
+    save_path = make_path3()
+    if not os.path.isdir(os.path.join(config['checkpoint_base'], save_path)):
+        os.makedirs(os.path.join(config['checkpoint_base'], save_path), exist_ok=True)
+    model_name = os.path.join(config['checkpoint_base'], save_path, 'best_lr={}.pt'.format(opts.lr))
+    torch.save(model.state_dict(), model_name)
+def save_mlp(model):
+    save_path = make_path2()
+    if not os.path.isdir(os.path.join(config['checkpoint_base'], save_path)):
+        os.makedirs(os.path.join(config['checkpoint_base'], save_path), exist_ok=True)
+    model_name = os.path.join(config['checkpoint_base'], save_path, 'best_lr={}mlp.pt'.format(opts.lr))
+    torch.save(model.state_dict(), model_name)
+
+def load_model(model, ckpt_path = None):
+    if ckpt_path is not None:
+        model_name = ckpt_path
+    else:
+        load_path = make_path()
+        if not os.path.isdir(os.path.join(config['checkpoint_base'], load_path)):
+            os.makedirs(os.path.join(config['checkpoint_base'], load_path), exist_ok=True)
+        model_name = os.path.join(config['checkpoint_base'], load_path, 'best_lr={}.pt'.format(opts.lr))
+        
+    print('load checkpoint from %s'%model_name)
+    checkpoint = torch.load(model_name, map_location='cpu') 
+    state_dict = checkpoint
+    msg = model.load_state_dict(state_dict,strict=False)
+    print("missing keys:", msg.missing_keys)
+
+    return model 
+
+
+def preload_model(model):
+
+    state_dict = torch.load(opts.preload_path, map_location=model.device) 
+    msg = model.load_state_dict(state_dict,strict=False)
+    print("missing keys:", msg.missing_keys)
+
+    return model 
+
+
+""" ==================== Tools ======================== """
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def makedir(path):
+    if not os.path.exists(path):
+        os.makedirs(path, 0o777)
+
+def visualizer():
+    if get_rank() == 0:
+        # filewriter_path = config['visual_base']+opts.savepath+'/'
+        save_path = make_path()
+        filewriter_path = os.path.join(config['visual_base'], save_path)
+        if opts.clear_visualizer and os.path.exists(filewriter_path):   # 删掉以前的summary，以免重合
+            shutil.rmtree(filewriter_path)
+        makedir(filewriter_path)
+        writer = SummaryWriter(filewriter_path, comment='visualizer')
+        return writer
